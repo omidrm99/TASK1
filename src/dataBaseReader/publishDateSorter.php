120156f4e9690e652d34ace170b4adea48faeab7
Your Name <you@example.com>
2024-03-02 21:56:08 +0330

    trying to clean up my mess after merging branches

--- src/dataBaseReader/publishDateSorter.php
+++ src/dataBaseReader/publishDateSorter.php
@@ -5,5 +5,22 @@ namespace App\dataBaseReader;
 class publishDateSorter
 {
 
+    private array $sorted = [];
+
+    public function __construct(array $mergedData)
+    {
+
+
+        usort($mergedData, function ($a, $b) {
+            return strtotime($a['publishDate']) - strtotime($b['publishDate']);
+        });
+
+        $this->sorted= $mergedData;
+    }
+
+    public function getSortedData(): array
+    {
+        return array_values($this->sorted);
+    }
 
 }
