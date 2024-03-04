4d9ad2fb981e9dceef33657c272e8593a85e743a
Your Name <you@example.com>
2024-03-04 20:29:38 +0330

    command reader file created

---
+++ src/request/CommandReader.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\request;
+
+class CommandReader
+{
+    private $command_name;
+    private $parameters;
+
+    public function __construct($json_file) {
+        $json_data = file_get_contents($json_file);
+        $data = json_decode($json_data, true);
+
+        if ($data === null) {
+            throw new Exception("Error decoding JSON");
+        }
+
+        $this->command_name = $data['command_name'];
+        $this->parameters = $data['parameters'];
+    }
+
+    public function getCommandName() {
+        return $this->command_name;
+    }
+
+    public function getParameters() {
+        return $this->parameters;
+    }
+
+    public function getISBNs() {
+        return $this->parameters['isbns'];
+    }
+}
